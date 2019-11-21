239aa5e8950860cdf43b3ef21af15c99931da35a
whitepurple <rofetist@gmail.com>
2019-11-21 10:09:22 +0900

    commit all
    commit all data and doing data

--- MakeTotalExcel/src/toTxtFile.java
+++ MakeTotalExcel/src/toTxtFile.java
@@ -4,9 +4,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
 
-//웹크롤링을 통해 얻어낸 CSV형식의 파일을 txt확장자로 파일 저장.
-//txt파일로 저장한 파일을 엑셀에서 데이터 텍스트로 불러와 유니코드 문자열로 저장
-//UTF-16LE사용
 
 public class toTxtFile {
 	private static toTxtFile uniqueInstance = null;
